a5bc5b8ce8448e4051fb993d86a01438a352dc9e
lgorse <lgorse@stanford.edu>
2013-06-30 15:48:08 -0700

    intermediate commit prior to shift to wikization branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
 	before_filter :authenticate
 
 	def show
-		if @current_user.account.has_active_motion
-			@motion = @current_user.account.active_motion
-		else
-			@motion = Motion.new
-		end
+		@motion = Motion.new
 	end
 end
