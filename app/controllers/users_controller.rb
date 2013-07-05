78d60fb2d5a46914ed099662c57c0a8c2a59431c
lgorse <lgorse@stanford.edu>
2013-07-05 12:55:26 -0700

    Finalized CSV batch members and created new user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 	require 'csv'
 	
 	before_filter :authenticate, :except => [:create_batch_members]
-	before_filter :authenticate_admin, :only => [:create_batch_members]
+	before_filter :authenticate_admin, :only => [:create_batch_members, :new, :create]
 
 	def create_batch_members
 		@account = Account.find(params[:account_id])
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
 		else
 			@main_message = "General error."
 		end
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(:name => params[:user][:name],
+						 :account_id => params[:user][:account],
+						 :team => params[:user][:team], 
+						 :email => params[:user][:email])
+		if @user.save
+			redirect_to user_path(@user)
+		else
+			render 'new'
+		end
+
 	end
 
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -87,6 +87,133 @@ describe UsersController do
 
 	end
 
-	
+	describe 'GET "new"' do
+
+		describe "if the user is NOT an admin" do
+			before(:each) do
+				@user = FactoryGirl.create(:user, :admin => false)
+				test_sign_in(@user)
+				get :new
+			end
+
+			it "should not be successful" do
+				response.should_not be_successful
+			end
+
+			it "should redirect the user to the root page" do
+				response.should redirect_to root_path
+
+			end
+
+		end
+
+		describe "if the user is an admin" do
+			before(:each) do
+				@user = FactoryGirl.create(:user, :admin => true)
+				test_sign_in(@user)
+				get :new
+			end
+
+			it "should be successful" do
+				response.should be_successful
+			end
+
+
+			it "should have a name input" do
+				response.body.should have_field('user[name]')
+			end
+
+			it "should have an e-mail input" do
+				response.body.should have_field('user[email]')
+			end
+
+			it "should have a team input" do
+				response.body.should have_field('user[team]')
+			end
+
+			it "should have an admin checkbox" do
+				response.body.should have_field('user[admin]')
+			end
+
+			it 'should have the current user account as a hidden field' do
+				response.body.should have_selector("input#user_account[value=\'"+@user.account_id.to_s+"\']")
+			end
+
+		end
+
+	end
+
+	describe 'POST "create"' do
+
+		describe "if user is not an admin" do
+
+			before(:each) do
+				@user = FactoryGirl.create(:user, :admin => false)
+				test_sign_in(@user)
+				@attr = {:name => "tester", :email => "test@tester.edu", :admin => false,  :team => "test", :account => @user.account_id}
+				post :create, :user => @attr
+			end
+
+			it "should not be successful" do
+				response.should_not be_successful
+			end
+
+			it "should redirect the user to the root page" do
+				response.should redirect_to root_path
+
+			end
+
+		end
+
+		describe "if user is an admin" do
+			before(:each) do
+				@user = FactoryGirl.create(:user, :admin => true)
+				test_sign_in(@user)
+			end
+
+			describe "if the input is well formed" do
+				before(:each) do
+					@attr = {:name => "tester", :email => "test75@tester.edu", :admin => false,  :team => "test", :account => @user.account_id}
+				end
+
+				it "should create a new user" do
+					lambda do
+						post :create, :user => @attr
+					end.should change(User, :count).by(1)
+				end
+
+				it "should redirect to the new user page" do
+					post :create, :user => @attr
+					response.should redirect_to user_path(assigns(:user))
+				end
+
+			end
+
+			describe "if the input is badly formed" do
+				before(:each) do
+					@attr = {:name => "tester", :email => "test@tester", :admin => false,  :team => "test", :account => @user.account_id}
+					post :create, :user => @attr
+				end
+
+				it "should not create a new user" do
+					lambda do
+						post :create, :user => @attr
+					end.should_not change(User, :count)
+
+				end
+
+				it "should re-render the new page to show error messages" do
+					post :create, :user => @attr
+					response.should render_template('new')
+				end
+			end
+
+		end
+
+
+
+	end
+
 
 end
+
