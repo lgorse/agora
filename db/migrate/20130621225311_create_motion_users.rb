897b520dc76809ca283712802fee3880b305efa8
lgorse <lgorse@stanford.edu>
2013-07-08 18:34:22 -0700

    removed the drop-table for motion_user

--- db/migrate/20130621225311_create_motion_users.rb
+++ db/migrate/20130621225311_create_motion_users.rb
@@ -1,6 +1,6 @@
 class CreateMotionUsers < ActiveRecord::Migration
   def change
-  	drop_table :motion_users
+  	
     create_table :motion_users do |t|
       t.integer :noise_id
       t.integer :user_id
