e24a058ccf43c0a4c9175e924748a47b22a439c4
lgorse <lgorse@stanford.edu>
2013-07-10 01:01:28 -0700

    implemented proper expiration of retries on workers and removed motions_helper short expiration

--- app/helpers/motions_helper.rb
+++ app/helpers/motions_helper.rb
@@ -3,7 +3,6 @@ module MotionsHelper
 	def expiration_times
 		from_time = Time.now
 		expiration_times = [
-			[distance_of_time_in_words(from_time, from_time + 1.minute), Time.now.since(1.minute)],
 		 [distance_of_time_in_words(from_time, from_time + 1.hour), Time.now.since(1.hour)],
 		 [distance_of_time_in_words(from_time, from_time + 6.hours), Time.now.since(6.hours)],
 		 [distance_of_time_in_words(from_time, from_time + 1.day), Time.now.since(1.day)],
--- app/workers/expiration_worker.rb
+++ app/workers/expiration_worker.rb
@@ -1,5 +1,6 @@
 class ExpirationWorker
 	include Sidekiq::Worker
+	sidekiq_options :retry => 5
 
 	def perform(motion_id)
 		motion = Motion.find(motion_id)
