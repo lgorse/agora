dcd406c0909f4a23a5cd1a82b032ce88668bf42f
lgorse <lgorse@stanford.edu>
2013-07-28 21:39:47 -0700

    implemented working invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -10,7 +10,7 @@ class InvitationsController < ApplicationController
 
 	def create
 		@valid_emails, @invalid_emails = parse_emails(params[:email_list])
-
-
+		@valid_emails.each {|email| Invitation.create(:inviter_id => @current_user.id, :account_id => @account.id, 
+									:email => email, :message => params[:message])}
 	end
 end
--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -1,5 +1,78 @@
 require 'spec_helper'
 
 describe InvitationsController do
+	render_views
+
+	describe 'GET /new' do
+		
+		before(:each) do
+			@user = FactoryGirl.create(:user)
+			test_sign_in(@user)
+		end
+
+		it "should be successful" do
+			get :new
+			response.should be_successful
+		end
+
+		it "should have an e-mail field" do
+			get :new
+			response.body.should have_field('email_list')
+		end
+
+		it "should have a message field" do
+			get :new
+			response.body.should have_field("message")
+		end
+
+		describe "when there are email params" do
+			before(:each) do
+				@emails = "stuff, super.cool"
+				get :new, :emails => @emails
+			end
+
+			it "should fill the email field with the emails" do
+				response.body.should have_field('email_list', :text => @emails)
+			end
+
+		end
+
+	end
+
+	describe 'POST /create' do
+		before(:each) do
+			@emails = "super@hotdog.com, power@crunch.buns, buns, print"
+			@message = "hello, world"
+			@user = FactoryGirl.create(:user)
+			test_sign_in(@user)
+		end
+
+		it "should differentiate valid e-mails from invalid e-mails" do
+			post :create, :email_list => @emails, :message => @message
+			assigns(:valid_emails).should_not include("print")
+		end
+
+		it "should create new invitations from the valid e-mails" do
+			post :create, :email_list => @emails, :message => @message
+			assigns(:valid_emails).each do |email|
+				Invitation.find_by_email(email).should_not == nil
+			end
+		end
+
+		it "should not create invitations from the invalid e-mails " do
+			post :create, :email_list => @emails, :message => @message
+			assigns(:invalid_emails).each do |email|
+				Invitation.find_by_email(email).should == nil
+			end
+		end
+
+		it "should start async jobs to e-mail invitees" do
+			lambda do
+				post :create, :email_list => @emails, :message => @message
+			end.should change(InviteWorker.jobs, :size).by(2)
+
+		end
+
+	end
 
 end
