9be612c4f3e7a3306e317aee37fc4da0b9a03c3d
lgorse <lgorse@stanford.edu>
2013-07-31 12:26:17 -0700

    Implemented working beautified reply button

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -5,9 +5,14 @@ module PagesHelper
 				:expires_at => Time.now.since(1.hour)}
 		motion_complain = Motion.new(attributes.merge(:title => "Let\'s keep the studio clean", 
 									 				  :details => "Especially the week-old food in the fridge!"))
+		motion_complain.id = 1
 		motion_idea = Motion.new(attributes.merge(:title => 'Should we do a regular Friday happy hour?',
-												  :details => "I feel we need to know each other better"))
+												  :details => "I feel we need to know each other better",
+												  :anonymous => false))	
+		motion_idea.id = 2
 		@example_motions = [motion_complain, motion_idea]
+		@example_motions.each {|motion| motion.created_at = Time.now.ago(1.hour)}
+
 
 	end
 end
